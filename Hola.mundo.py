342102fa147d99a2825d2720776e9b5595027480
Nicolas Moreno <nicolas.morenob@upb.edu.co>
2026-01-23 15:00:32 -0500

    iCAMBIOS PYTHON

--- Hola.mundo.py
+++ Hola.mundo.py
@@ -1,3 +1,16 @@
 # MI PRIMER SCRIPT.PY
 print ("Hola Mundo!")
+n = 1000
+es_primo = [True] * (n + 1)
+es_primo[0] = es_primo[1] = False
+
+for i in range(2, int(n**0.5) + 1):
+    if es_primo[i]:
+        for j in range(i * i, n + 1, i):
+            es_primo[j] = False
+
+primos = [i for i in range(2, n + 1) if es_primo[i]]
+
+print("Números primos entre 1 y 1000:")
+print(primos)
 
